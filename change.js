91371683ce079ee9e72ffd46a0d835cf89695161
Mike Manchee <mikemanchee@gmail.com>
2020-11-09 10:37:18 -0800

    debug coin couter part 1
    co-authored-by: Brycen Bartolome <brycenbartolome@gmail.com>

--- change.js
+++ change.js
@@ -11,22 +11,18 @@ function divide(input) {
   const total = change(amount, 0);
   return total;
 }
-
+S
 function change(amount, cent) {
   if (cent <= 3) {
-    console.log(cent);
     const i = Math.floor(amount/array[cent]);
     const left = amount - (i * array[cent])
     if( i === 1 ) {
-      console.log("one");
       array1[cent] = `${i} ${word[cent]}`;
     }
     else if ( i > 1 ) {
-      console.log("more");
       array1[cent] = `${i} ${words[cent]}`;
     }
     else {}
-    console.log(array1);
     change(left, cent + 1)
   }
    return array1;
